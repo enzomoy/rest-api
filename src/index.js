ec61e77b88a3557e9fec815407cf8144b3c0b897
Enzo <enzo.moy94@gmail.com>
2024-04-26 08:34:17 +0200

    edit docker-compose

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connexion à la base de données
-sequelize.sync().then(() => {
-  console.log('La connexion à la base de données a été établie avec succès.');
-  const port = 8000;
-  app.listen(port, () => {
-    console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+  sequelize.sync().then(() => {
+    console.log('La connexion à la base de données a été établie avec succès.');
+    const port = 8000;
+    app.listen(port, () => {
+      console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
+    });
+  }).catch((error) => {
+    console.error('Erreur lors de la connexion à la base de données:', error);
   });
-}).catch((error) => {
-  console.error('Erreur lors de la connexion à la base de données:', error);
-});
 
 // Importation des routes
 const userRoute = require("./backend/routes/user.route");
